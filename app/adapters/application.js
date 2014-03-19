e96a48740ed9b3e8be0d188f736157f42f78bd36
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2014-03-19 15:09:38 -0400

    Confirm app works while proxying to rails backend

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,7 @@
 import Book from "appkit/models/book";
 
 //export default DS.FixtureAdapter.extend();
+
 export default DS.LSAdapter.extend({});
 
 var namespace = 'DS.LSAdapter';
@@ -14,6 +15,7 @@ if(!localStorage.getItem(namespace)) {
   localStorage.setItem(namespace, JSON.stringify(data));
 }
 
-//export default DS.RESTAdapter.extend({
-//  namespace: 'api'
-//});
+/*export default DS.RESTAdapter.extend({
+  namespace: 'api'
+});
+*/
