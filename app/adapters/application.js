a8d1e479cf6fad2139824695caf7e2eb08cd991c
Adam Ferguson <Adam.Brugh.Ferguson@gmail.com>
2014-03-19 14:03:49 -0400

    Change emblem file extensions. Add support for localstorage-adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,19 @@
-export default DS.FixtureAdapter.extend();
+import Book from "appkit/models/book";
+
+//export default DS.FixtureAdapter.extend();
+export default DS.LSAdapter.extend({});
+
+var namespace = 'DS.LSAdapter';
+if(!localStorage.getItem(namespace)) {
+  var data = {book: {records: {}}};
+
+  Book.FIXTURES.forEach(function(book) {
+    data.book.records[book.id] = book;
+  });
+
+  localStorage.setItem(namespace, JSON.stringify(data));
+}
+
 //export default DS.RESTAdapter.extend({
 //  namespace: 'api'
 //});
